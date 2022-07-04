487a106b64474c5aa8dfb4f2d9369d98c95a38df
Alex Milkov <amilkov@squareup.com>
2022-07-04 15:15:35 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='finBERT',
+      version='1.0',
+      description='foo',
+      author='foo',
+      author_email='foo',
+      url='foo',
+      packages=['finbert', 'notebooks'],
+     )
